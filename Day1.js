1f0097a9717ef99701a609a9e940bb31ad03b863
Khanjan nath <161429455+khanjan2024@users.noreply.github.com>
2024-07-13 19:30:09 +0530

    Update Day1.js

--- Day1.js
+++ Day1.js
@@ -1,1 +1,59 @@
+// Activity 1
+//Task 1
+var firstVariable=7;
+console.log(firstVariable);
+//Task 2
+let str="Messi is the G.O.A.T";
+console.log(str);
+
+
+
+
+// Activity 2
+//Task 3
+const booleanVal=true;
+console.log(booleanVal);
+
+
+
+//Activity 3
+//Task 4
+const myProfile={
+    fullName :"Jimmy Anderson",
+    followers:500,
+    isFollow:true,
+};
+
+console.log(typeof myProfile);
+console.log(typeof myProfile["fullName"]);
+console.log(typeof myProfile["followers"]);
+console.log(typeof myProfile["isFollow"]);
+
+
+let marks=[59,68,89];
+console.log(typeof marks);
+
+
+
+
+//Activity 4
+//Task 5
+let initialVariable=78;
+console.log("Intial value is :",initialVariable);
+initialVariable=87;
+console.log("Intial value is :",initialVariable);
+
+
+
+//Activity 5
+//Task 6
+const newVariable=67;
+console.log("Intial value is :",newVariable);
+try{
+    newVariable=61;
+}catch(error){
+    console.error("Error :"+error.message)
+}
+
+
 
