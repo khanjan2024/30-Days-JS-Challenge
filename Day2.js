76b28b1cf0593ab1844f49f8ed2010704949384b
Khanjan nath <161429455+khanjan2024@users.noreply.github.com>
2024-07-14 22:40:23 +0530

    Update Day2.js

--- Day2.js
+++ Day2.js
@@ -1,1 +1,64 @@
+//Activity 1
+let num1=34;
+num2=18;
+//Task 1
+res=num1+num2;
+console.log("The addition of the two number is :",res);
+//Task 2
+res=num1-num2;
+console.log("The substraction of the two number is :",res);
+//Task 3
+res=num1*num2;
+console.log("The multiplication of the two number is :",res);
+//Task 4
+res=num1/num2;
+console.log("The divison of the two number is :",res);
+//Task 5
+res=num1 % num2;
+console.log("The remainder  is :",res);
 
+
+
+//Activity 2
+//Task 6
+let number1=67;
+number1+=34;
+console.log(number1);
+//Task 7
+let number2=98;
+number2-= 23;
+console.log(number2);
+
+//Activity 3
+//Task 8
+let a=76;
+let b=90;
+console.log("a greater than b is :",a>b);
+console.log("a less than b is :",a<b);
+//Task 9
+let n1=89;
+let n2=87;
+console.log("n1 greater than or equal to n2",n1>=n2);
+console.log("n1 less than or equal to n2",n1<=n2);
+//Task 10
+let firstNum=23;
+let secondNum='23';
+console.log("Using == :",firstNum==secondNum);
+console.log("Using === :",firstNum===secondNum);
+
+
+//Activity 4
+//Task 11
+let cond1=29;
+let cond2=89;
+console.log(cond1>cond2 && cond1<=cond2);
+//Task 12
+console.log(cond1>cond2 || cond1<=cond2);
+//Task 13
+console.log(cond1 !== cond2);
+
+//Activity 5
+//Task 15
+let ternOpr=0;
+let result=ternOpr>=0?"Positive":"Negative";
+console.log("The number is :",result);
